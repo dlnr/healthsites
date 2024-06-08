0b7788b953b8fe3af61b36333a4cbe5f921974d5
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-06-09 01:46:21 +0200

    No hashing

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
 	  input: {
         main: resolve(__dirname, 'src/index.html'),
         map: resolve(__dirname, 'src/map/index.html'),
+      },
+      output: {
+        entryFileNames: `js/[name].js`,
+        chunkFileNames: `js/[name].js`,
+        assetFileNames: 'assets/[name].[ext]',
       }
-      // output: {
-      //   entryFileNames: `js/[name].js`,
-      //   chunkFileNames: `js/[name].js`,
-      //   assetFileNames: 'css/[name].css',
-      // }
     },
     outDir: '../public',
   	sourcemap: true,
