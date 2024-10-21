6ec9e09aaf683c31fc4bdc27e2eac0800288bb49
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-10-21 20:27:21 +0200

    Scaffold idea of dynamic user stories

---
+++ src/userstories.js
@@ -0,0 +1,28 @@
+const geo = "Ethiopia";
+
+const stories = [
+	epidemiologist = [
+		{
+				"goal": "diagnostic services",
+				"result": "I can see the number of cases per day",
+				"URL": "/api/public/facilities/cluster?bbox=32.54150390625%2C-0.8788717828324148%2C48.44970703125%2C18.979025953255267&zoom=6&iconsize=48%2C46&geoname=Ethiopia&tag=&spec=&data=&uuid=&filters=%7B%22amenity%22%3A%5B%22clinic%22%5D%7D"
+		},
+		{
+				"goal": "diagnostic services",
+				"result": "I can see the number of cases per day",
+				"URL": "/api/public/facilities/cluster?bbox=32.54150390625%2C-0.8788717828324148%2C48.44970703125%2C18.979025953255267&zoom=6&iconsize=48%2C46&geoname=Ethiopia&tag=&spec=&data=&uuid=&filters=%7B%22amenity%22%3A%5B%22clinic%22%5D%7D"
+		},
+	],
+	PregnantMom = [
+		{
+				"goal": "diagnostic services",
+				"result": "I can see the number of cases per day",
+				"URL": "/api/public/facilities/cluster?bbox=32.54150390625%2C-0.8788717828324148%2C48.44970703125%2C18.979025953255267&zoom=6&iconsize=48%2C46&geoname=Ethiopia&tag=&spec=&data=&uuid=&filters=%7B%22amenity%22%3A%5B%22clinic%22%5D%7D"
+		},
+		{
+				"goal": "diagnostic services",
+				"result": "I can see the number of cases per day",
+				"URL": "/api/public/facilities/cluster?bbox=32.54150390625%2C-0.8788717828324148%2C48.44970703125%2C18.979025953255267&zoom=6&iconsize=48%2C46&geoname=Ethiopia&tag=&spec=&data=&uuid=&filters=%7B%22amenity%22%3A%5B%22clinic%22%5D%7D"
+		},
+	]
+]
